583bfbd04142c3880e1001f4cd2d2ba3ab05aaff
baldhat <baldhat@web.de>
2021-11-16 08:23:02 +0100

    task: Added orphan removal

--- src/main/kotlin/com/msd/planet/domain/Planet.kt
+++ src/main/kotlin/com/msd/planet/domain/Planet.kt
@@ -1,8 +1,10 @@
 package com.msd.planet.domain
 
+import com.msd.robot.domain.Robot
 import java.util.*
 import javax.persistence.Entity
 import javax.persistence.Id
+import javax.persistence.OneToMany
 
 @Entity
 data class Planet(
@@ -11,4 +13,8 @@ data class Planet(
 ) {
 
     var blocked: Boolean = false
+
+    // just for orphan removal
+    @OneToMany(mappedBy = "robot", orphanRemoval = true)
+    private val robots: List<Robot> = listOf()
 }
