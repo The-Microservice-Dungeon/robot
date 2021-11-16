8f48a49baea4b88c4d0b68b386242f8a1734283d
Dennis <denwae@tutanota.com>
2021-11-16 20:42:04 +0100

    Revert "test: added useRepairItem test"
    This reverts commit 2b669fb52dfff22f7c82fb3adeeb6b364b4cf879.

--- src/test/kotlin/com/msd/robot/domain/RobotDomainServiceTest.kt
+++ src/test/kotlin/com/msd/robot/domain/RobotDomainServiceTest.kt
@@ -1,7 +1,6 @@
 package com.msd.robot.domain
 
 import com.msd.domain.ResourceType
-import com.msd.item.domain.ReparationItemType
 import com.msd.planet.domain.Planet
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
@@ -169,36 +168,6 @@ internal class RobotDomainServiceTest {
                     assertEquals(1, robot5.inventory.getStorageUsageForResource(it))
                 }
             }
-        )
-    }
-
-    @Test
-    fun `using repair swarm heals all robots of the player owning the robot`() {
-        // given
-        robot3.move(planet2, 0)
-        val player1Robots = listOf(robot1, robot2, robot3)
-        player1Robots.forEach {
-            it.upgrade(UpgradeType.HEALTH)
-            it.repair()
-            it.receiveDamage(21)
-        }
-
-        every { robotRepository.findByIdOrNull(robot1.id) } returns robot1
-        every {
-            robotRepository.findAllByPlayerAndPlanet_PlanetId(
-                robot1.player,
-                robot1.planet.planetId
-            )
-        } returns player1Robots
-        every { robotRepository.saveAll(player1Robots) } returns player1Robots
-
-        // when
-        robotDomainService.useReparationItem(robot1.id, robot1.player, ReparationItemType.REPARATION_SWARM.func)
-
-        // then
-        assertAll(
-            "all robots of player1 healed for 20 health",
-            robots.filter { it.player == player1Id }.map { { assertEquals(24, it.health) } }
         )
     }
 }
