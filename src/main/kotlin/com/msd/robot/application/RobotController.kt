46812e24c0fae3a0fdf8b21f0415564707fcc0f5
baldhat <baldhat@web.de>
2021-11-20 18:52:09 +0100

    task: renamed attirbutes

--- src/main/kotlin/com/msd/robot/application/RobotController.kt
+++ src/main/kotlin/com/msd/robot/application/RobotController.kt
@@ -3,7 +3,6 @@ package com.msd.robot.application
 import com.msd.domain.ResourceType
 import com.msd.robot.application.dtos.*
 import com.msd.robot.domain.RobotDomainService
-import org.springframework.data.repository.query.Param
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
@@ -36,7 +35,7 @@ class RobotController(
      * @return A List of [RobotDto]s
      */
     @GetMapping
-    fun getRobotsOfPlayer(@Param("player-id") playerId: UUID): ResponseEntity<List<RobotDto>> {
+    fun getRobotsOfPlayer(@RequestParam("player-id") playerId: UUID): ResponseEntity<List<RobotDto>> {
         val robots = robotDomainService.getRobotsByPlayer(playerId)
         return ResponseEntity.ok(robotMapper.robotsToRobotDtos(robots))
     }
@@ -61,7 +60,7 @@ class RobotController(
      */
     @PostMapping("/{id}/upgrades")
     fun upgradeRobot(@PathVariable("id") robotId: UUID, @RequestBody upgradeDto: UpgradeDto): ResponseEntity<String> {
-        robotApplicationService.upgrade(robotId, upgradeDto.`upgrade-type`, upgradeDto.`target-level`)
+        robotApplicationService.upgrade(robotId, upgradeDto.upgradeType, upgradeDto.targetLevel)
         return ResponseEntity.ok("") // TODO remove target-level and return new level in DTO
     }
 
@@ -76,8 +75,8 @@ class RobotController(
         @PathVariable("id") robotId: UUID,
         @RequestBody addItemDto: ItemAdditionDto
     ): ResponseEntity<String> {
-        robotDomainService.addItem(robotId, addItemDto.`item-type`)
-        return ResponseEntity.ok("Item ${addItemDto.`item-type`} added to robot $robotId")
+        robotDomainService.addItem(robotId, addItemDto.itemType)
+        return ResponseEntity.ok("Item ${addItemDto.itemType} added to robot $robotId")
     }
 
     /**
