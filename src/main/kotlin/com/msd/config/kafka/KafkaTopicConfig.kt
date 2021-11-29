995b8bb83d872d62dcc1cda94b30e14cecc6eabc
Dennis <denwae@tutanota.com>
2021-11-29 15:01:33 +0100

    chore: added new topics

--- src/main/kotlin/com/msd/config/kafka/KafkaTopicConfig.kt
+++ src/main/kotlin/com/msd/config/kafka/KafkaTopicConfig.kt
@@ -13,8 +13,32 @@ class KafkaTopicConfig {
     @Value(value = "\${spring.kafka.bootstrap-servers}")
     private lateinit var bootstrapAddress: String
 
-    @Value(value = "\${spring.kafka.topic.producer.robot}")
-    private lateinit var robotTopic: String
+    @Value(value = "\${spring.kafka.topic.producer.robot-movement}")
+    private lateinit var movementTopic: String
+
+    @Value(value = "\${spring.kafka.topic.producer.robot-neighbours}")
+    private lateinit var neighboursTopic: String
+
+    @Value(value = "\${spring.kafka.topic.producer.robot-planet-blocked}")
+    private lateinit var planetBlockedTopic: String
+
+    @Value(value = "\${spring.kafka.topic.producer.robot-movement}")
+    private lateinit var miningTopic: String
+
+    @Value(value = "\${spring.kafka.topic.producer.robot-topic}")
+    private lateinit var fightingTopic: String
+
+    @Value(value = "\${spring.kafka.topic.producer.robot-regeneration}")
+    private lateinit var regenerationTopic: String
+
+    @Value(value = "\${spring.kafka.topic.producer.robot-item-fighting}")
+    private lateinit var itemFightingTopic: String
+
+    @Value(value = "\${spring.kafka.topic.producer.robot-item-repair}")
+    private lateinit var itemRepairTopic: String
+
+    @Value(value = "\${spring.kafka.topic.producer.robot-item-movement}")
+    private lateinit var itemMovementTopic: String
 
     @Value(value = "\${spring.kafka.topic.consumer.round}")
     private lateinit var roundTopic: String
@@ -26,8 +50,32 @@ class KafkaTopicConfig {
     }
 
     @Bean
-    fun robotTopic(): NewTopic = NewTopic(robotTopic, 1, 1)
+    fun movementTopic(): NewTopic = NewTopic(movementTopic, 1, 1)
 
     @Bean
-    fun prodRoundTopic(): NewTopic = NewTopic(roundTopic, 1, 1)
+    fun neighboursTopic(): NewTopic = NewTopic(neighboursTopic, 1, 1)
+
+    @Bean
+    fun planetBlockedTopic(): NewTopic = NewTopic(planetBlockedTopic, 1, 1)
+
+    @Bean
+    fun miningTopic(): NewTopic = NewTopic(miningTopic, 1, 1)
+
+    @Bean
+    fun fightingTopic() = NewTopic(fightingTopic, 1, 1)
+
+    @Bean
+    fun regenerationTopic() = NewTopic(regenerationTopic, 1, 1)
+
+    @Bean
+    fun itemFightingTopic() = NewTopic(itemFightingTopic, 1, 1)
+
+    @Bean
+    fun itemRepairTopic() = NewTopic(itemRepairTopic, 1, 1)
+
+    @Bean
+    fun itemMovementTopic() = NewTopic(itemMovementTopic, 1, 1)
+
+    @Bean
+    fun roundTopic(): NewTopic = NewTopic(roundTopic, 1, 1)
 }
