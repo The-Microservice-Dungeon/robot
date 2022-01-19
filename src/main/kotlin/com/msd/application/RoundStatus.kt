ba3fd2d90a8efaec9dd253c4e1003c416980dbee
baldhat <baldhat@web.de>
2022-01-19 21:28:21 +0100

    Fixed roundSTatus Consumer Enum

--- src/main/kotlin/com/msd/application/RoundStatus.kt
+++ src/main/kotlin/com/msd/application/RoundStatus.kt
@@ -1,7 +1,12 @@
 package com.msd.application
 
-enum class RoundStatus(val value: String) {
-    STARTED("started"),
-    COMMAND_INPUT_ENDED("command input ended"),
-    ENDED("ended")
+import com.fasterxml.jackson.annotation.JsonProperty
+
+enum class RoundStatus {
+    @JsonProperty("started")
+    STARTED,
+    @JsonProperty("command input ended")
+    COMMAND_INPUT_ENDED,
+    @JsonProperty("ended")
+    ENDED
 }
