5edf240ff33390d5a5f61d909981423926dd82c9
baldhat <baldhat@web.de>
2022-01-19 21:54:57 +0100

    added roundId to RoundStatusDTO.kt

--- src/main/kotlin/com/msd/application/dto/RoundStatusDTO.kt
+++ src/main/kotlin/com/msd/application/dto/RoundStatusDTO.kt
@@ -1,5 +1,6 @@
 package com.msd.application.dto
 
 import com.msd.application.RoundStatus
+import java.util.*
 
-class RoundStatusDTO(val roundNumber: Int, val roundStatus: RoundStatus)
+class RoundStatusDTO(val roundNumber: Int, val roundStatus: RoundStatus, val roundId: UUID)
