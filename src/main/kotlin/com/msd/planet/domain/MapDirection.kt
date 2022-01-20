50d86eab27389a09e3ac0a82148ceebc1ac8f157
baldhat <baldhat@web.de>
2022-01-20 10:58:14 +0100

    Change MapDirection json properties

--- src/main/kotlin/com/msd/planet/domain/MapDirection.kt
+++ src/main/kotlin/com/msd/planet/domain/MapDirection.kt
@@ -1,8 +1,14 @@
 package com.msd.planet.domain
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 enum class MapDirection {
+    @JsonProperty("north")
     NORTH,
+    @JsonProperty("south")
     SOUTH,
+    @JsonProperty("east")
     EAST,
+    @JsonProperty("west")
     WEST
 }
