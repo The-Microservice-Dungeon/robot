120567b57bb56bdc6f19909317684765693a217d
baldhat <baldhat@web.de>
2022-01-22 21:30:35 +0100

    Added logging of the command execution mode

--- src/main/kotlin/com/msd/command/application/CommandController.kt
+++ src/main/kotlin/com/msd/command/application/CommandController.kt
@@ -29,11 +29,12 @@ class CommandController(
     fun receiveCommand(@RequestBody commandDto: CommandDTO): ResponseEntity<Any> {
         if (commandDto.commands.isNotEmpty()) {
             val commands = commandService.parseCommandsFromStrings(commandDto.commands)
-            logger.info("Starting execution of command batch")
             if (!environment.acceptsProfiles("no-async")) {
+                logger.info("Starting execution of command batch asynchronously")
                 val thread = Thread { robotService.executeCommands(commands) }
                 thread.start()
             } else {
+                logger.info("Starting execution of command batch synchronously")
                 robotService.executeCommands(commands)
             }
         }
