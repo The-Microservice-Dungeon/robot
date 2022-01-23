7cfbcae29191026060e1dd8a94e53aca6a2aab31
baldhat <baldhat@web.de>
2022-01-23 17:03:25 +0100

    Added JsonProperty for ResourceType-enum

--- src/main/kotlin/com/msd/domain/ResourceType.kt
+++ src/main/kotlin/com/msd/domain/ResourceType.kt
@@ -1,9 +1,16 @@
 package com.msd.domain
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 enum class ResourceType(val requiredMiningLevel: Int) {
+    @JsonProperty("coal",)
     COAL(0),
+    @JsonProperty("iron")
     IRON(1),
+    @JsonProperty("gem")
     GEM(2),
+    @JsonProperty("gold")
     GOLD(3),
+    @JsonProperty("platin")
     PLATIN(4)
 }
