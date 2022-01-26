a22f9ccba52bbc9d8291e5a2a366123f63768e79
Dennis <denwae@tutanota.com>
2022-01-26 14:23:40 +0100

    fix: added missing gameId to RoundStatusDTO

--- src/main/kotlin/com/msd/application/dto/RoundStatusDTO.kt
+++ src/main/kotlin/com/msd/application/dto/RoundStatusDTO.kt
@@ -3,4 +3,4 @@ package com.msd.application.dto
 import com.msd.application.RoundStatus
 import java.util.*
 
-class RoundStatusDTO(val roundNumber: Int, val roundStatus: RoundStatus, val roundId: UUID)
+class RoundStatusDTO(val roundNumber: Int, val roundStatus: RoundStatus, val roundId: UUID, val gameId: UUID)
